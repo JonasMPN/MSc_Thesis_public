9cf80679d3480bc095dab1ff718f3a1b7b3a1a58
Jonas MPN <you@example.com>
2024-04-18 16:04:28 +0200

    - add comments - change inflow definition

--- section_aeroelastics/main.py
+++ section_aeroelastics/main.py
@@ -10,12 +10,12 @@ do = {
     "plot_results": True  # plot results
 }
 
-root = "data/NACA_643_618"
+root = "data/NACA_643_618"  # set which airfoil polars to use. Simulatenously defines to root for the simulation data
 
 if do["simulate"]:
-    file_polar_data = join(root, "polars.dat")
-    density = 1
-    struct_def = {
+    file_polar_data = join(root, "polars.dat")  
+    density = 1  # density of the fluid
+    struct_def = {  # definition of structural parameters
         "chord": 1,
         "mass": 1,
         "mom_inertia": 1,
@@ -26,37 +26,39 @@ if do["simulate"]:
         "stiffness_flap": 1,
         "stiffness_tors": 1,
     }
-    t = np.linspace(0, 40, 1300)
+    t = np.linspace(0, 40, 1300)  # set the time array for the simulation
     NACA_643_618 = ThreeDOFsAirfoil(file_polar_data, t, **struct_def)
 
-    inflow_speed = 1*np.ones((t.size, 1))
+    # the following lines define the inflow at each time step
+    inflow_speed = 1*np.ones((t.size, 1)) 
     t_angle_ramp = 3
     n_steps_angle_change = int(np.ceil(t_angle_ramp/(max(t)/t.size)))
-    inflow_angle = 50*np.ones(t.size)
-    inflow_angle[:n_steps_angle_change] = np.linspace(90, 50, n_steps_angle_change)
+    inflow_angle = 40*np.ones(t.size)  # in degrees
+    inflow_angle[:n_steps_angle_change] = np.linspace(0, inflow_angle[0], n_steps_angle_change)
     inflow_angle = np.deg2rad(inflow_angle)
-    inflow = inflow_speed*np.c_[np.sin(inflow_angle), np.cos(inflow_angle)]
+    inflow = inflow_speed*np.c_[np.cos(inflow_angle), np.sin(inflow_angle)]
 
-    init_pos = np.zeros(3)
-    init_vel = np.zeros(3)
+    init_pos = np.zeros(3)  # initial conditions for the position in [x, y, rotation in rad]
+    init_vel = np.zeros(3)  # initial conditions for the velocity in [x, y, rotation in rad/s]
 
-    NACA_643_618.set_aero_calc()
-    NACA_643_618.set_struct_calc()
-    NACA_643_618.set_time_integration()
-    NACA_643_618.simuluate(inflow, density, init_pos, init_vel)
-    NACA_643_618.save(join(root, "simulation"))
+    NACA_643_618.set_aero_calc()  # set the calculation scheme for the aerodynamic forces
+    NACA_643_618.set_struct_calc()  # set the calculation scheme for the structural damping and stiffness forces
+    NACA_643_618.set_time_integration()  # set the time integration scheme
+    NACA_643_618.simuluate(inflow, density, init_pos, init_vel)  # perform simulation
+    NACA_643_618.save(join(root, "simulation"))  # save simulation results
 
 if do["post_calc"]:
     post_calc = PostCaluculations(dir_sim_res=join(root, "simulation"), alpha_lift="alpha_qs")
-    post_calc.project_data()
-    post_calc.work()
-    post_calc.kinetic_energy()
-    post_calc.potential_energy()
+    post_calc.project_data()  # projects the simulation's x-y-rot_z data into different coordinate systems such as
+    # edgewise-flapwise-rot_z or drag-lift_rot_z.
+    post_calc.work()  # calculate and save the work done by the aerodynamic and structural damping forces
+    post_calc.kinetic_energy()  # calculate the edgewise, flapwise, and rotational kinetic energy
+    post_calc.potential_energy()  # calculate the edgewise, flapwise, and rotational potential energy
 
 if do["plot_results"]:
-    file_profile = join(root, "profile.dat")
-    dir_sim = join(root, "simulation")
-    dir_plots = join(dir_sim, "plots")
-    plotter = Plotter(file_profile, dir_sim, dir_plots)
-    plotter.force()
-    plotter.energy()
+    file_profile = join(root, "profile.dat")  # define path to file containing the profile shape data
+    dir_sim = join(root, "simulation")  # define path to the root of the simulation results
+    dir_plots = join(dir_sim, "plots")  # define path to the directory that the results are plotted into
+    plotter = Plotter(file_profile, dir_sim, dir_plots) 
+    plotter.force()  # plot various forces of the simulation
+    plotter.energy()  # plot various energies and work done by forces of the simulation
