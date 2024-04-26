469f215626d5f708bf98bd84af81594058ce7d43
Jonas MPN <you@example.com>
2024-04-26 10:36:19 +0200

    begin work on HHT_alpha validation

--- section_aeroelastics/validation.py
+++ section_aeroelastics/validation.py
@@ -4,10 +4,11 @@ import pandas as pd
 import numpy as np
 from os import listdir
 from os.path import join
-from calculations import ThreeDOFsAirfoil, AeroForce
+from calculations import ThreeDOFsAirfoil, AeroForce, TimeIntegration, StructForce
+from post_calculations import PostCaluculations
 import numpy as np
 from utils_plot import MosaicHandler
-from defaults import DefaultsPlots
+from defaults import DefaultsPlots, DefaultStructure
 from helper_functions import Helper
 import json
 helper = Helper()
@@ -65,16 +66,16 @@ class ValidationPlotter(DefaultsPlots):
             ):
         dir_plots = helper.create_dir(join(dir_preparation, "plots"))[0]
         df_section = pd.read_csv(join(dir_preparation, "sep_points.dat"))
-        df_aerohor = pd.read_csv(join(dir_preparation, "f_lookup_1.dat"))
+        # df_aerohor = pd.read_csv(join(dir_preparation, "f_lookup_1.dat"))
         df_polar = pd.read_csv(file_polar, delim_whitespace=True)
-        alpha_0_aerohor = pd.read_csv(join(dir_preparation, "alpha_0.dat"))["alpha_0"].to_numpy()
-        alpha_0_aerohor = np.deg2rad(alpha_0_aerohor)
+        # alpha_0_aerohor = pd.read_csv(join(dir_preparation, "alpha_0.dat"))["alpha_0"].to_numpy()
+        # alpha_0_aerohor = np.deg2rad(alpha_0_aerohor)
         with open(join(dir_preparation, "alpha_0.json"), "r") as f:
             alpha_0_section = np.deg2rad(json.load(f)["alpha_0"])
             
         for f_type in ["f_t", "f_n"]:
             fig, ax = plt.subplots()
-            ax.plot(df_aerohor["alpha_n"], df_aerohor[f"{f_type}"], **self.plot_settings[f"{f_type}_aerohor"])
+            # ax.plot(df_aerohor["alpha_n"], df_aerohor[f"{f_type}"], **self.plot_settings[f"{f_type}_aerohor"])
             ax.plot(df_section["alpha"], df_section[f"{f_type}"], 
                     **self.plot_settings[f"{f_type}_section"])
             handler = MosaicHandler(fig, ax)
@@ -82,9 +83,9 @@ class ValidationPlotter(DefaultsPlots):
             handler.save(join(dir_plots, f"{f_type}_model_comp.pdf"))
         
         rot = Rotations()
-        coeffs_aerohor = np.c_[self._get_C_t(alpha_0_aerohor, df_aerohor["alpha_t"], df_aerohor["f_t"]),
-                               self._get_C_n(alpha_0_aerohor, df_aerohor["alpha_n"], df_aerohor["f_n"])]
-        rot_coeffs_aerohor = rot.rotate_2D(coeffs_aerohor, np.deg2rad(df_aerohor["alpha_n"].to_numpy()))
+        # coeffs_aerohor = np.c_[self._get_C_t(alpha_0_aerohor, df_aerohor["alpha_t"], df_aerohor["f_t"]),
+        #                        self._get_C_n(alpha_0_aerohor, df_aerohor["alpha_n"], df_aerohor["f_n"])]
+        # rot_coeffs_aerohor = rot.rotate_2D(coeffs_aerohor, np.deg2rad(df_aerohor["alpha_n"].to_numpy()))
         
         coesffs_section = np.c_[self._get_C_t(alpha_0_section, df_section["alpha"], df_section["f_t"]),
                                 self._get_C_n(alpha_0_section, df_section["alpha"], df_section["f_n"])]
@@ -94,8 +95,8 @@ class ValidationPlotter(DefaultsPlots):
             sign = 1 if coeff_type == "C_l" else -1
             fig, ax = plt.subplots()
             ax.plot(df_polar["alpha"], df_polar[coeff_type], **self.plot_settings[f"{coeff_type}_meas"])
-            ax.plot(df_aerohor["alpha_n"], sign*rot_coeffs_aerohor[:, i], 
-                    **self.plot_settings[f"{coeff_type}_rec_aerohor"])
+            # ax.plot(df_aerohor["alpha_n"], sign*rot_coeffs_aerohor[:, i], 
+            #         **self.plot_settings[f"{coeff_type}_rec_aerohor"])
             ax.plot(df_section["alpha"], sign*rot_coeffs_section[:, i], 
                     **self.plot_settings[f"{coeff_type}_rec_section"])
             handler = MosaicHandler(fig, ax)
@@ -106,7 +107,7 @@ class ValidationPlotter(DefaultsPlots):
     @staticmethod
     def plot_model_comparison(dir_results: str):
         dir_plots = helper.create_dir(join(dir_results, "plots", "model_comp"))[0]
-        df_aerohor = pd.read_csv(join(dir_results, "aerohor_res.dat"))
+        # df_aerohor = pd.read_csv(join(dir_results, "aerohor_res.dat"))
         df_section_general = pd.read_csv(join(dir_results, "general.dat"))
         df_section = pd.read_csv(join(dir_results, "f_aero.dat"))
 
@@ -139,12 +140,12 @@ class ValidationPlotter(DefaultsPlots):
             "f_n": r"$f_{\text{n}}$ (-)", 
             "f_t": r"$f_{\text{t}}$ (-)", 
         }
-        t_aerohor = df_aerohor["t"]
+        # t_aerohor = df_aerohor["t"]
         t_section = df_section_general["time"]
         for plot_param in plot_model_compare_params:
             fig, ax = plt.subplots()
             handler = MosaicHandler(fig, ax)
-            ax.plot(t_aerohor, df_aerohor[plot_param], **plt_settings["aerohor"])
+            # ax.plot(t_aerohor, df_aerohor[plot_param], **plt_settings["aerohor"])
             ax.plot(t_section, df_section[plot_param], **plt_settings["section"])
             handler.update(x_labels="t (s)", y_labels=y_labels[plot_param], legend=True)
             handler.save(join(dir_plots, f"model_comp_{plot_param}.pdf"))
@@ -163,7 +164,7 @@ class ValidationPlotter(DefaultsPlots):
         period_res: int,
         ):
         df_meas = pd.read_csv(file_unsteady_data, delim_whitespace=True)
-        df_aerohor = pd.read_csv(join(dir_results, "aerohor_res.dat"))
+        # df_aerohor = pd.read_csv(join(dir_results, "aerohor_res.dat"))
         df_section = pd.read_csv(join(dir_results, "f_aero.dat"))
         # map_measurement = {"C_d": " Cdp", "C_l": " Cl"}
         map_measurement = {"alpha": "AOA", "C_l": "CL", "C_d": "CD"}
@@ -182,8 +183,8 @@ class ValidationPlotter(DefaultsPlots):
             handler = MosaicHandler(fig, ax)
             ax.plot(df_meas[map_measurement["alpha"]], df_meas[map_measurement[coef]], 
                     **self.plot_settings[coef+"_meas"])
-            ax.plot(df_aerohor["alpha_steady"][-period_res-1:], df_aerohor[coef][-period_res-1:], 
-                    **plt_settings["section"])
+            # ax.plot(df_aerohor["alpha_steady"][-period_res-1:], df_aerohor[coef][-period_res-1:], 
+            #         **plt_settings["section"])
             ax.plot(np.rad2deg(df_section["alpha_steady"][-period_res-1:]), df_section[coef][-period_res-1:], 
                     **plt_settings["aerohor"])
             handler.update(x_labels=r"$\alpha_{\text{steady}}$ (°)", y_labels=rf"${{{coef[0]}}}_{{{coef[2]}}}$ (-)",
@@ -199,12 +200,207 @@ class ValidationPlotter(DefaultsPlots):
         return C_l_slope*np.sin(np.deg2rad(alpha)-alpha_0)*((1+np.sqrt(np.abs(f_n))*np.sign(f_n))/2)**2
 
 
+class HHT_alpha_validator(DefaultsPlots, DefaultStructure):
+    def __init__(self, dir_polar: str, dir_root_results: str) -> None:
+        DefaultsPlots.__init__(self)
+        DefaultStructure.__init__(self)
+        self.dir_polar = dir_polar
+        self.dir_root_results = dir_root_results
+        self._struc_params = None
+        self._response_wanted = None
+
+    def set_structure(
+            self,
+            chord: float,
+            mass: float=None,
+            mom_inertia: float=None,
+            stiffness_edge: float=None,
+            stiffness_flap: float=None,
+            stiffness_tors: float=None,
+            damping_edge: float=None,
+            damping_flap: float=None,
+            damping_tors: float=None,
+    ):
+        skip = ["self", "chord", "mass", "mom_inertia"]
+        struc_params = {param: val for param, val in locals().items() if param not in ["skip"]+skip}
+        response_wanted = {i: [] for i in range(3)}
+        for i, (param, val) in enumerate(struc_params.items()):
+            if val == None:
+                struc_params[param] = 1
+                response_wanted[i%3].append(False)
+            else:
+                response_wanted[i%3].append(True)
+        self._response_wanted = {i: any(val_give) for i, val_give in response_wanted.items()}
+        if (self._response_wanted[0] or self._response_wanted[1]) and mass is None:
+            raise ValueError("'mass' must be set when calculating the response in the edgewise or flapwise direction.")
+        if self._response_wanted[2] and mom_inertia is None:
+            raise ValueError("'mom_inertia' must be set when calculating the response in the torsional direction.")
+        
+        self._struc_params = struc_params|{"chord": chord, 
+                                           "mass": mass if mass is not None else 1, 
+                                           "mom_inertia": mom_inertia if mom_inertia is not None else 1}
+        self._struct_force = StructForce(**struc_params)
+
+    def simulate(
+            self,
+            case_name: str,
+            alpha: float,
+            time: np.ndarray,
+            init_pos: np.ndarray,
+            init_vel: np.ndarray,
+            init_accel: np.ndarray,
+            f_edge: np.ndarray=None,
+            f_flap: np.ndarray=None,
+            f_tors: np.ndarray=None
+    ):
+        use = ["f_edge", "f_flap", "f_tors"]
+        force = {param: val for param, val in locals().items() if param in use}
+        f_ext = np.zeros((time.size, 3))
+        f_response_wanted = {i: True for i in range(3)}
+        for i, (param, val) in enumerate(force.items()):
+            if val is None:
+                force[param] = 1
+                f_response_wanted[i] = False
+            else:
+                f_ext[:, i] = val
+        for i, direction in enumerate(["edgewise", "flapwise", "torsional"]):
+            if self._response_wanted[i] and not f_response_wanted[i]:
+                raise ValueError(f"{direction} force time series is missing.")
+            elif not self._response_wanted[i] and f_response_wanted[i]:
+                raise ValueError(f"{direction} structural parameter(s) is(are) missing.")
+            
+        dir_save = helper.create_dir(join(self.dir_root_results, case_name))[0]
+        airfoil = ThreeDOFsAirfoil(dir_polar=self.dir_polar, time=time, **self._struc_params)
+        airfoil.pos[0, :] = init_pos
+        airfoil.vel[0, :] = init_vel
+        airfoil.accel[0, :] = init_accel
+        
+        airfoil.aero = f_ext
+        time_integrator = TimeIntegration()
+        time_integrator._init_HHT_alpha(airfoil, alpha, t[1])
+        airfoil.damp = np.zeros((t.size, 3))
+        airfoil.stiff = np.zeros((t.size, 3))
+
+        for i in range(t.size-1):
+            pos, vel, accel = time_integrator._HHT_alpha(airfoil, i, t[1])
+            # print(airfoil.pos)
+            airfoil.pos[i+1, :] = pos
+            airfoil.vel[i+1, :] = vel
+            airfoil.accel[i+1, :] = accel
+
+            #todo check coordinate sytsem under rotation (also check for force)
+            airfoil.damp[i, :], airfoil.stiff[i, :] = self._struct_force._linear(airfoil, i)
+            
+        i = t.size-1
+        airfoil.damp[i, :], airfoil.stiff[i, :] = self._struct_force._linear(airfoil, i)
+        airfoil.inflow = np.zeros((time.size, 2))
+        airfoil._save(dir_save)
+        with open(join(dir_save, "section_data.json"), "w") as f:
+            json.dump(self._struc_params, f, indent=4)
+
+        post_calc = PostCaluculations(dir_save, "alpha_steady")
+        post_calc.project_data()
+        post_calc.work()
+        post_calc.kinetic_energy()
+        post_calc.potential_energy()
+
+    def plot(
+        self, 
+        case_name: str, 
+        solution: dict[str, np.ndarray], 
+        directions_wanted: list=["edge", "flap", "tors"],
+        error_type: str="absolute"):
+        dir_res = join(self.dir_root_results, case_name, "plots")
+        df_general = pd.read_csv(join(dir_res, self._dfl_filenames["general"])) 
+        time = df_general["time"]
+        df_f_aero = pd.read_csv(join(dir_res, self._dfl_filenames["f_aero"]))
+        df_f_struct = pd.read_csv(join(dir_res, self._dfl_filenames["f_structural"]))
+        df_work = pd.read_csv(join(dir_res, self._dfl_filenames["work"]))
+        df_e_kin = pd.read_csv(join(dir_res, self._dfl_filenames["e_kin"]))
+        df_e_pot = pd.read_csv(join(dir_res, self._dfl_filenames["e_pot"]))
+
+        axs_labels_pos = [["pos", "aero"],
+                          ["damp", "stiff"]] 
+        fig, axs = plt.subplot_mosaic(axs_labels_pos, tight_layout=True)
+        fig_err, axs_err = plt.subplot_mosaic(axs_labels_pos, tight_layout=True)
+        y_labels = {"pos": "position (m), (°)", "aero": r"$f_{\text{external}}$ (N), (Nm)",
+                    "damp": r"$f_{\text{damping}}$ (N), (Nm)", "stiff": r"$f_{\text{stiffness}}$ (N), (Nm)"}
+        y_labels_err = {ax: f"{error_type} error {label[:label.find("(")-1]}" for ax, label in y_labels.items()}
+
+        dfs = {"pos": df_general, "aero": df_f_aero, "damp": df_f_struct, "stiff": df_f_struct}
+        legend = self._plot(axs, axs_err, dfs, time, directions_wanted, error_type)
+        handler = MosaicHandler(fig, axs)
+        handler.update(legend=legend, y_labels=y_labels, x_labels="time (s)")
+        handler.save(join(dir_res, "pos.pdf"))
+        
+        handler_err = MosaicHandler(fig_err, axs_err)
+        handler_err.update(legend=legend, y_labels=y_labels_err, x_labels="time (s)")
+        handler_err.save(join(dir_res, "error_pos.pdf"))
+
+        fig, axs = plt.subplot_mosaic([["total", "work"],
+                                       ["e_kin", "e_pot"]], tight_layout=True)
+        y_labels = {"total": "Energy (Nm)", "work": "Work (Nm)",
+                   "e_kin": f"Kinetic energy (Nm)", "e_pot": "Potential energy (Nm)"}
+        y_labels_err = {ax: f"{error_type} error {label[:label.find("(")-1]}" for ax, label in y_labels.items()}
+        df_total = pd.concat([df_e_kin.sum(axis=1), df_e_pot.sum(axis=1)], keys=["e_kin", "e_pot"], axis=1)
+        df_total["e_total"] = df_total.sum(axis=1)
+        dfs = {"total": df_total, "work": df_work, "e_kin": df_e_kin, "pot": df_e_pot}
+        directions_wanted += ["kin", "pot", "total"]
+        legend = self._plot(axs, axs_err, dfs, time, directions_wanted, error_type)
+
+        handler = MosaicHandler(fig, axs)
+        handler.update(legend=legend, y_labels=y_labels, x_labels="time (s)")
+        handler.save(join(dir_res, "energy.pdf"))
+
+        handler_err = MosaicHandler(fig_err, axs_err)
+        handler_err.update(legend=legend, y_labels=y_labels_err, x_labels="time (s)")
+        handler_err.save(join(dir_res, "error_energy.pdf"))
+    
+    @staticmethod
+    def _plot(
+        axs: dict[str], 
+        axs_err: dict[str], 
+        dfs: dict[str, pd.DataFrame], 
+        time: np.ndarray,
+        directions_wanted: list[str],
+        error_type: str="absolute"):
+        legend = {}
+        for (param_type, ax), ax_err in zip(axs.items(), axs_err.values()):
+            df = dfs[param_type]
+            for col in df.columns:
+                correct_type = param_type in col
+                col_of_interest = np.count_nonzero(df[col])
+                correct_direction = any([direction in col for direction in directions_wanted])
+                if not all([correct_type, col_of_interest, correct_direction]):
+                    continue
+                try:
+                    solution[col]
+                except KeyError:
+                    raise KeyError(f"Solution missing for '{col}'. Add those to solution['{col}'].")
+                label = col[col.rfind("_")+1:]
+                ax.plot(time, df[col], label=label)
+                ax.plot(time, solution[col], "k", linestyle="--", label="sol")
+                match error_type:
+                    case "absolute":
+                            ax_err.plot(time, solution[col]-df[col], label=label)
+                    case "relative":
+                            ax_err.plot(time, (solution[col]-df[col])/solution[col], label=label)
+                    case _:
+                            raise ValueError(f"'error_type' can be 'absolute' or 'relative' but was {error_type}.")
+                legend[param_type] = True
+        return legend
+    
+
 if __name__ == "__main__":
     do = {
-        "run_simulation": True,
-        "plot_results": True
+        "run_simulation": False,
+        "plot_results": False,
+        "calc_HHT_alpha_response": True,
+        "plot_HHT_alpha_response": True
     }
     dir_airfoil = "data/FFA_WA3_221"
+    dir_HHT_alpha_validation = "data/HHT_alpha_validation"
+    HHT_alpha_case = "test"
     period_res = 100
 
     if do["run_simulation"]:
@@ -224,4 +420,47 @@ if __name__ == "__main__":
             plotter.plot_model_comparison(dir_case)
             plotter.plot_meas_comparison(join(dir_airfoil, "unsteady", df_cases["filename"].iat[int(case_name)]),
                                          dir_case, period_res=period_res)
+        
+    if do["calc_HHT_alpha_response"] or do["plot_HHT_alpha_response"]:
+        T = 10
+        dt = 0.1
+        t = dt*np.arange(T/dt+1)
+        alpha = 0.1
 
+        init_pos = (1, 0, 0)
+        init_vel = (0, 0, 0)
+        init_accel = (0, 0, 0)
+
+        force_edge = np.zeros_like(t)
+        force_flap = None
+        force_tors = None
+
+        chord = 1
+        mass = .1
+        mom_inertia = None
+
+        stiffness_edge = 1
+        stiffness_flap = None
+        stiffness_tors = None
+
+        damping_edge = 0
+        damping_flap = None
+        damping_tors = None
+
+    if do["calc_HHT_alpha_response"]:
+        validator = HHT_alpha_validator(dir_airfoil, dir_HHT_alpha_validation)
+        validator.set_structure(chord=chord,mass=mass,mom_inertia=mom_inertia,stiffness_edge=stiffness_edge,
+                                stiffness_flap=stiffness_flap,stiffness_tors=stiffness_tors,damping_edge=damping_edge,
+                                damping_flap=damping_flap,damping_tors=damping_tors)   
+        validator.simulate(case_name=HHT_alpha_case, time=t, alpha=alpha, init_pos=init_pos,init_vel=init_vel,
+                           init_accel=init_accel, f_edge=force_edge, f_flap=force_flap, f_tors=force_tors)
+        
+    if do["plot_HHT_alpha_response"]:
+        validator = HHT_alpha_validator(dir_airfoil, dir_HHT_alpha_validation)
+        
+        solution = {
+            "pos_edge": np.cos(np.sqrt(stiffness_edge/mass)*t),
+            "stiff_edge": -np.cos(np.sqrt(stiffness_edge/mass)*t),
+            # "e_total": 
+        }
+        validator.plot(case_name=HHT_alpha_case, solution=solution)
